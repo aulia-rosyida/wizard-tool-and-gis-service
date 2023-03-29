a5bd0c7734ce05d1ae4a878de6d0715b6e415319
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-29 09:19:12 +0700

    feat: add 2 button navigation

--- wizard-and-gis-by-aulia/src/components/StepperControl.jsx
+++ wizard-and-gis-by-aulia/src/components/StepperControl.jsx
@@ -3,19 +3,23 @@ import React from 'react'
 const StepperControl = () => {
     return (
         <div className='container flex justify-around mt-4 mb-8'>
-            <button className='bg-white text-slate-400 uppercase py-2 px-4 roundex-xl font-semibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
+            <button className='btn btn-white'>
                 Back
             </button>
 
 
-            <button className='bg-green-500 text-white uppercase py-2 px-4 roundex-xl font-semibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
+
+            <button class="btn btn-blue">
                 Next
             </button>
+
+
 
         </div>
 
 
     )
 }
+
 
 export default StepperControl
