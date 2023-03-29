e7d06d81638b4e30ba50faf601ea6dab526784b2
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-29 21:10:58 +0700

    feat: fe form step 1

--- wizard-and-gis-by-aulia/src/pages/wizard/FormData.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/FormData.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid, TextField, MenuItem } from '@material-ui/core';
+import Datepicker from "react-tailwindcss-datepicker";
+import './Styles.css';
 
 export default function FormData() {
 
+    const countries = [
+        { value: "Indonesia", label: "Indonesia" },
+        { value: "Cambodia", label: "Cambodia" },
+        { value: "Laos", label: "Laos" },
+    ];
+
+    const [value, setValue] = useState({
+        startDate: new Date(),
+        endDate: new Date().setMonth(11)
+    });
+    const handleValueChange = (newValue) => {
+        console.log("newValue:", newValue);
+        setValue(newValue);
+    }
+
     return (
-        <div className="container mx-auto bg-white rounded-xl shadow border p-8 m-10">
+        <div className="container mx-auto bg-white rounded-xl shadow border p-8 m-10" >
             <p className="text-3xl text-gray-700 font-bold mb-5 text-center">
                 Form Data
             </p>
-            <p className="text-gray-500 text-lg text-center">
-                Created by Aulia Rosyida
-            </p>
-            <p className="text-3xl text-gray-700 text-center pt-12">
-                Create Proposal Document
-            </p>
-
-        </div>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 10, sm: 20, md: 30 }}>
+                <Grid item xs={6} className="pb-4 pr-4">
+                    <TextField fullWidth select id="outlined-select" label="Indonesia" variant="outlined" className="mx-480 my-120 w-1/2">
+                        {countries.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs={6} className="pb-4 pr-4">
+                    <Datepicker
+                        placeholderText='DD/MM/YYYY'
+                        dateFormat='dd/MM/yyyy'
+                        primaryColor={"blue"}
+                        value={value}
+                        onChange={handleValueChange}
+                        showShortcuts={true}
+                        background="bg-white-700 light:bg-white-800"
+                    />
+                </Grid>
+                <Grid item xs={6} className="pb-4 pr-4">
+                    <TextField fullWidth id="outlined-basic" label="Name of the Area" variant="outlined" />
+                </Grid>
+            </Grid>
+        </div >
     )
 }
+
+// export default FormData
