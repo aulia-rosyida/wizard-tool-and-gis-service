59f70dafed794c95fad3bcd57c0fbd7843b7766a
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-29 15:43:35 +0700

    feat: hide button back in first step

--- wizard-and-gis-by-aulia/src/components/StepperControl.jsx
+++ wizard-and-gis-by-aulia/src/components/StepperControl.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const StepperControl = ({ handleClick, currentStep, steps }) => {
     return (
         <div className='container flex justify-around mt-4 mb-8'>
-            <button className={`btn btn-white ${currentStep === 1 ? "opacity-50 cursor-not-allowed" : ""}`} onClick={() => handleClick("previous")}>
-                Back
-            </button>
+            {currentStep > 1 && < button className={`btn btn-white`} onClick={() => handleClick("previous")}>Back</button>}
             <button class={`btn  ${currentStep === steps.length ? "btn-dark-blue" : "btn-blue"}`} onClick={() => handleClick("next")}>
                 {currentStep === steps.length ? "Export" : "Next"}
             </button>
-        </div>
+        </div >
     )
 }
 
