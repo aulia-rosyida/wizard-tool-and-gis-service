d726aece4bc3c1b905dac1539d652c2a08972006
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-29 15:17:31 +0700

    feat: screen default for each step

---
+++ wizard-and-gis-by-aulia/src/pages/wizard/Review.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Review() {
+
+    return (
+        <div className="container mx-auto bg-white rounded-xl shadow border p-8 m-10">
+            <p className="text-3xl text-gray-700 font-bold mb-5 text-center">
+                Review
+            </p>
+            <p className="text-gray-500 text-lg text-center">
+                Created by Aulia Rosyida
+            </p>
+            <p className="text-3xl text-gray-700 text-center pt-12">
+                Create Proposal Document
+            </p>
+
+        </div>
+    )
+}
