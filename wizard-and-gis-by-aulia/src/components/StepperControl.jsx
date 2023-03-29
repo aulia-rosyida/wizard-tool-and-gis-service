bfdf5354eedf2ed3be356e8aadef872c28b9aee6
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-29 15:17:53 +0700

    feat: button handling - next and back

--- wizard-and-gis-by-aulia/src/components/StepperControl.jsx
+++ wizard-and-gis-by-aulia/src/components/StepperControl.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const StepperControl = () => {
+const StepperControl = ({ handleClick, currentStep, steps }) => {
     return (
         <div className='container flex justify-around mt-4 mb-8'>
-            <button className='btn btn-white'>
+            <button className={`btn btn-white ${currentStep === 1 ? "opacity-50 cursor-not-allowed" : ""}`} onClick={() => handleClick("previous")}>
                 Back
             </button>
-            <button class="btn btn-blue">
-                Next
+            <button class={`btn  ${currentStep === steps.length ? "btn-dark-blue" : "btn-blue"}`} onClick={() => handleClick("next")}>
+                {currentStep === steps.length ? "Export" : "Next"}
             </button>
         </div>
     )
