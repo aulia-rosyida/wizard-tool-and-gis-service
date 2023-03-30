a66f8a5312c45f9c73f937877c78862450e5d1e1
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-30 20:20:33 +0700

    feat: handle warning classname

--- wizard-and-gis-by-aulia/src/components/StepperControl.jsx
+++ wizard-and-gis-by-aulia/src/components/StepperControl.jsx
@@ -4,7 +4,7 @@ const StepperControl = ({ handleClick, currentStep, steps }) => {
     return (
         <div className='container flex justify-around mt-4 mb-8'>
             {currentStep > 1 && < button className={`btn btn-white`} onClick={() => handleClick("previous")}>Back</button>}
-            <button class={`btn  ${currentStep === steps.length ? "btn-dark-blue" : "btn-blue"}`} onClick={() => handleClick("next")}>
+            <button className={`btn  ${currentStep === steps.length ? "btn-dark-blue" : "btn-blue"}`} onClick={() => handleClick("next")}>
                 {currentStep === steps.length ? "Export" : "Next"}
             </button>
         </div >
