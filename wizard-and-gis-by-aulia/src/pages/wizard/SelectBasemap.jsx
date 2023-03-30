1d5b68a881f4010ba9b3b376add7528935b7a320
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-30 11:13:51 +0700

    feat: styling image

--- wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
@@ -8,9 +8,9 @@ function Option({ name, id, imagePath }) {
             <input class="hidden" id={id} type="radio" name="radio" checked />
             <label class="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" for={id}>
                 <span class="text-xl font-bold mt-2 text-center pb-4">{name}</span>
-                {id == "osm" ? <img class="rounded-lg w-48 h-24" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="OpenStreetMap" /> : <div />}
-                {id == "esri" ? <img class="rounded-lg w-48 h-24" src="https://www.esri.com/about/newsroom/wp-content/uploads/2018/09/1213-tip-8.jpg" alt="Esri Map" /> : <div />}
-                {id == "gmaps" ? <img class="rounded-lg w-48 h-24" src="https://www.google.com/maps/d/u/0/thumbnail?mid=13f7lbxmHclFswUm3k140oIU2-KI" alt="Google Map" /> : <div />}
+                {id == "osm" ? <img className="img-basemap" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="OpenStreetMap" /> : <div />}
+                {id == "esri" ? <img className="img-basemap" src="https://www.esri.com/about/newsroom/wp-content/uploads/2018/09/1213-tip-8.jpg" alt="Esri Map" /> : <div />}
+                {id == "gmaps" ? <img className="img-basemap" src="https://www.google.com/maps/d/u/0/thumbnail?mid=13f7lbxmHclFswUm3k140oIU2-KI" alt="Google Map" /> : <div />}
             </label>
         </div>
     );
@@ -25,7 +25,7 @@ export default function SelectBasemap() {
                 Select Basemap
             </p>
             <div class="flex items-center justify-center py-12">
-                <form class="grid grid-cols-3 gap-2 w-full max-w-screen-sm">
+                <form class="grid grid-cols-3 gap-8 w-full">
                     <Option name="OpenStreetMap" id="osm" />
                     <Option name="ESRI Map" id="esri" />
                     <Option name="Google Maps" id="gmaps" />
