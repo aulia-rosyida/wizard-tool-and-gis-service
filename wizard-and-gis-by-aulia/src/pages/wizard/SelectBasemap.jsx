36a37b1551fca2ad31ec4f8d1a99297e4288a7c1
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-30 22:30:59 +0700

    feat: handle warning - class to className

--- wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
@@ -5,8 +5,8 @@ import './Styles.css';
 function Option({ name, id, chosenId, onOptionChange }) {
     return (
         <div>
-            <input class="hidden" id={id} value={id} type="radio" name="radio" checked={id === chosenId} onChange={onOptionChange} />
-            <label class="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" for={id}>
+            <input className="hidden" id={id} value={id} type="radio" name="radio" checked={id === chosenId} onChange={onOptionChange} />
+            <label className="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" for={id}>
                 <span class="text-xl font-bold mt-2 text-center pb-4">{name}</span>
                 {id === "osm" ? <img className="img-basemap" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="OpenStreetMap" /> : <div />}
                 {id === "esri" ? <img className="img-basemap" src="https://www.esri.com/about/newsroom/wp-content/uploads/2018/09/1213-tip-8.jpg" alt="Esri Map" /> : <div />}
@@ -23,8 +23,8 @@ export default function SelectBasemap(props) {
             <p className="text-3xl text-gray-700 font-bold mb-5 text-center">
                 Select Basemap
             </p>
-            <div class="flex items-center justify-center py-12">
-                <form class="grid grid-cols-3 gap-8 w-full">
+            <div className="flex items-center justify-center py-12">
+                <form className="grid grid-cols-3 gap-8 w-full">
                     <Option name="OpenStreetMap" id="osm" chosenId={props.mapId} onOptionChange={props.onChoose} />
                     <Option name="ESRI Map" id="esri" chosenId={props.mapId} onOptionChange={props.onChoose} />
                     <Option name="Google Maps" id="gmaps" chosenId={props.mapId} onOptionChange={props.onChoose} />
