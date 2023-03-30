38d14dc8e776459afe28eb479a88d7fd493fac25
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-31 00:04:18 +0700

    feat: add image placeholder

--- wizard-and-gis-by-aulia/src/pages/wizard/Review.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/Review.jsx
@@ -16,8 +16,9 @@ export default function Review(props) {
             <p className="text-3xl text-gray-700 font-bold mb-5 text-center">
                 Review
             </p>
-            <div class="grid grid-cols-2 pl-20">
-                <img className="img-review object-top-right" src={props.screenshotHolder == null ? "https://osm.gs.mil/images/screenshots/slippymap.png" : props.screenshotHolder} alt="Map Screenshot" />
+            <div class="grid grid-cols-2 pl-20" id="report">
+                {/* <img className="img-review object-top-right" src={props.screenshotHolder == null ? "https://osm.gs.mil/images/screenshots/slippymap.png" : props.screenshotHolder} alt="Map Screenshot" /> */}
+                <img className="img-review object-top-right" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="Map Screenshot" />
                 <div className='pt-4'>
                     <TextReview title="Country" content={props.countryHolder.framework} />
                     <TextReview title="Area" content={props.areaHolder.current} />
