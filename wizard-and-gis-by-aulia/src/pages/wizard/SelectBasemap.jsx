f5cb71e425ade3e42339d665fa8f95a1f2a3300b
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-30 09:40:10 +0700

    feat: add image for select basemap (not done)

--- wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import './Styles.css';
 
-function Checklist() {
-    return <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" />
-        <path d="M5 12l5 5l10 -10" />
-    </svg>;
-}
 
-
-function Option({ name, id }) {
+function Option({ name, id, imagePath }) {
     return (
         <div>
             <input class="hidden" id={id} type="radio" name="radio" checked />
             <label class="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" for={id}>
-                <span class="text-xl font-bold mt-2 text-center">{name}</span>
-                <Checklist />
+                <span class="text-xl font-bold mt-2 text-center pb-4">{name}</span>
+                <img src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="Map Default" />
+                {/* <img href={require(imagePath)} alt="Map Default" /> */}
             </label>
         </div>
     );
@@ -31,9 +25,9 @@ export default function SelectBasemap() {
             </p>
             <div class="flex items-center justify-center py-12">
                 <form class="grid grid-cols-3 gap-2 w-full max-w-screen-sm">
-                    <Option name="OpenStreetMap" id="radio_1" />
-                    <Option name="ESRI Map" id="radio_2" />
-                    <Option name="Google Maps" id="radio_3" />
+                    <Option name="OpenStreetMap" id="osm" imagePath={'/src/assets/esri-maps.png'} />
+                    <Option name="ESRI Map" id="esri" imagePath={'/src/assets/esri-maps.png'} />
+                    <Option name="Google Maps" id="gmaps" imagePath={'/src/assets/esri-maps.png'} />
                 </form>
             </div>
         </div>
