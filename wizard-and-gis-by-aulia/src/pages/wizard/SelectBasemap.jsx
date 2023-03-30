b72bef8b5011a103245a26c6fa065e39fe6cedae
aulia-rosyida <rosyidaaulia24@gmail.com>
2023-03-30 12:11:40 +0700

    chore: add = for warning handle

--- wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
+++ wizard-and-gis-by-aulia/src/pages/wizard/SelectBasemap.jsx
@@ -8,9 +8,9 @@ function Option({ name, id, imagePath }) {
             <input class="hidden" id={id} type="radio" name="radio" checked />
             <label class="flex flex-col p-4 border-2 border-gray-400 cursor-pointer" for={id}>
                 <span class="text-xl font-bold mt-2 text-center pb-4">{name}</span>
-                {id == "osm" ? <img className="img-basemap" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="OpenStreetMap" /> : <div />}
-                {id == "esri" ? <img className="img-basemap" src="https://www.esri.com/about/newsroom/wp-content/uploads/2018/09/1213-tip-8.jpg" alt="Esri Map" /> : <div />}
-                {id == "gmaps" ? <img className="img-basemap" src="https://www.google.com/maps/d/u/0/thumbnail?mid=13f7lbxmHclFswUm3k140oIU2-KI" alt="Google Map" /> : <div />}
+                {id === "osm" ? <img className="img-basemap" src="https://osm.gs.mil/images/screenshots/slippymap.png" alt="OpenStreetMap" /> : <div />}
+                {id === "esri" ? <img className="img-basemap" src="https://www.esri.com/about/newsroom/wp-content/uploads/2018/09/1213-tip-8.jpg" alt="Esri Map" /> : <div />}
+                {id === "gmaps" ? <img className="img-basemap" src="https://www.google.com/maps/d/u/0/thumbnail?mid=13f7lbxmHclFswUm3k140oIU2-KI" alt="Google Map" /> : <div />}
             </label>
         </div>
     );
